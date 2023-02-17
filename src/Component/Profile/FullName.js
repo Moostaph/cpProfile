ae1b3006f11c78ffa5e393d9f57c151e622812fb
Moostaph <mbayemouha89@gmail.com>
2023-02-17 11:30:44 +0000

    dernier commit
    le dernier commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,5 @@
+import React from "react";
+const FullName = ()=> {
+    return <h1>Mouhamadou Moustapha Mbaye</h1>;
+};
+export default FullName;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,6 @@
+import React from "react";
+import profile from "./profile.png";
+const ProfilePhoto = ()=> {
+return<img src= {profile} />;
+};
+export default ProfilePhoto;
